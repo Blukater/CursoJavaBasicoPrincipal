9640aa52747bffb6d7329d162d65e817d6faf9df
j.filho <jfilhocobain13@hotmail.com>
2023-08-14 10:59:28 -0300

    até 20

--- src/treinando15/Ex19.java
+++ src/treinando15/Ex19.java
@@ -7,16 +7,63 @@ o 326 = 3 centenas, 2 dezenas e 6 unidades
 o 12 = 1 dezena e 2 unidades Testar com: 326, 300, 100, 320,
 310,305, 301, 101, 311, 111, 25, 20, 10, 21, 11, 1, 7 e 16
  */
-
 package treinando15;
 
 import java.util.Locale;
 import java.util.Scanner;
 
-
 public class Ex19 {
 
-public static void main(String[] args) {
-         Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+
+        int inteiro = 0;
+        while (true) {
+            System.out.println("Entre com um número inteiro menor que 1000:");
+            inteiro = scan.nextInt();
+
+            int unidade = 0;
+            int dezena = 0;
+            int centena = 0;
+
+            if (inteiro <= 1000) {
+
+                centena = inteiro / 100;
+                dezena = (inteiro / 10) % 10;
+                unidade = inteiro % 10;
+
+                System.out.println("Temos " + centena + "  centenas " + dezena + " dezenas e " + unidade + " unidades.");
+                break;
+            } else {
+                System.out.println("Número inválido, reinicie!");
+
+            }
+
+        }
+    }
 }
-}
+
+//OUTRA ALTERNATIVA
+/*int inteiro = 0;
+        while (true) {
+            System.out.println("Entre com um número inteiro menor que 1000:");
+            inteiro = scan.nextInt();
+
+            if (inteiro <= 1000 && inteiro >= 0) {
+                break; // Sai do loop se o número for válido
+            } else {
+                System.out.println("Número inválido, tente novamente!");
+            }
+        }
+
+        int[] unidades = new int[1];
+        int[] dezenas = new int[1];
+        int[] centenas = new int[1];
+
+        centenas[0] = inteiro / 100;
+        dezenas[0] = (inteiro / 10) % 10;
+        unidades[0] = inteiro % 10;
+
+        System.out.println("Temos " + centenas[0] + " centenas, " + dezenas[0] + " dezenas e " + unidades[0] + " unidades.");
+    }
+}*/
